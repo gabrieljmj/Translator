a3e89ab54c95bcb93613d1a5b5d4f16661af1ab3
GabrielJMJ <gamjj74@hotmail.com>
2014-07-14 16:39:35 -0300

    Fix tab spaces

--- src/Translator/Service/GoogleTranslate.php
+++ src/Translator/Service/GoogleTranslate.php
@@ -137,8 +137,8 @@
 
 			$ch = curl_init();
             
-            curl_setopt( $ch, CURLOPT_URL, $url );
-            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+			curl_setopt( $ch, CURLOPT_URL, $url );
+			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
             
 			$return = curl_exec( $ch );
             
